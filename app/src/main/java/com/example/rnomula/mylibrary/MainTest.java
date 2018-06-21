35b0a2b70fc74df1a7c438bd975bd527626d9d4d
Nomula,Raju <Raju.Nomula@chamberlain.com>
2018-06-21 14:25:57 -0500

    Maintest

--- app/src/main/java/com/example/rnomula/mylibrary/MainTest.java
+++ app/src/main/java/com/example/rnomula/mylibrary/MainTest.java
@@ -2,7 +2,7 @@ package com.example.rnomula.mylibrary;
 
 import android.util.Log;
 
-public class MainActivity {
+public class MainTest {
 
     public static void debug(String message) {
         Log.d("MainActivity", "message");
