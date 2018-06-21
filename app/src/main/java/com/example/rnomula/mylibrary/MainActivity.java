87e62d57b5e99bf764dff4a96e86c725e897916c
Nomula,Raju <Raju.Nomula@chamberlain.com>
2018-06-21 14:05:42 -0500

    Initilal commit

---
+++ app/src/main/java/com/example/rnomula/mylibrary/MainActivity.java
@@ -0,0 +1,11 @@
+package com.example.rnomula.mylibrary;
+
+import android.util.Log;
+
+public class MainActivity {
+
+    public static void debug(String message) {
+        Log.d("MainActivity", "message");
+    }
+}
+
